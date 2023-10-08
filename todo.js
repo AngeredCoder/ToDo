1c390b061fb30a0755f6680df89eb915273c5224
bmcbride <brianmcbride78@gmail.com>
2023-10-08 17:22:54 -0500

    Update todo.js

--- todo.js
+++ todo.js
@@ -11,10 +11,8 @@ $("ul").on("click", "span", function(event){
 
 $("input[type='text']").keypress(function(event){
 	if(event.which === 13){
-		//grabbing new todo text from input
 		var todoText = $(this).val();
 		$(this).val("");
-		//create a new li and add to ul
 		$("ul").append("<li><span><i class='far fa-trash-alt'></i></span>" + todoText + "</li>");
 	}
 });
